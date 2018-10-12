71614a5686eaeabad928eabeeddc93c6cab8aef0
AhmedMardiOsman <ahmedmardiosman@gmail.com>
2018-10-12 15:51:56 +0100

    first Commit

---
+++ Assesment_Week_2/src/main/java/com/qa/Assesment_Week_2/Gangster.java
@@ -0,0 +1,22 @@
+package com.qa.Assesment_Week_2;
+
+public class Gangster extends Charecter {
+
+	public Gangster( String charecterName, String charecterColour, int charecterAge, int charecterHeight) {
+		super(charecterName, charecterColour, charecterAge, charecterHeight);
+		
+	}
+	
+	public void gangsterCharecteristics() {
+		System.out.println("Your Gangsters name is " + charecterName);
+		System.out.println("Your Gangsters colour is " + charecterColour);
+		System.out.println("Your Gangsters Age is " + charecterAge);
+		System.out.println("Your Gangsters Height is " + charecterHeight + "cm");
+	}
+	
+	public void pistolWhip() {
+		System.out.println();
+	}
+	
+	
+}
